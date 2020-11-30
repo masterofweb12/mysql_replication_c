0b3ec32965b292eb9f5e973cc96cf357443a5086
Andrii Hlukhov <masterofweb12@gmail.com>
2020-11-30 14:17:38 +0200

    fix example build

--- example/C/build_test.sh
+++ example/C/build_test.sh
@@ -5,6 +5,18 @@ if [ -f ./rpl_test_2 ]; then
     rm ./rpl_test_2
 fi
 
+if [ -f ../../mysql_rpl_listener.so ]; then
+
+echo "shared library present"
+
+else
+
+cd ../../
+./build.sh
+cd example/C/
+
+fi
+
 gcc -v -g -O0 `mysql_config --cflags` \
  -o ./rpl_test_2 ./rpl_test_2.c \
  -lm -lrt -ldl  -L../../ -lmysql_rpl_listener  `mysql_config --libs` -lpthread -ldl -Wl,-rpath,../../
