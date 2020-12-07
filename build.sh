236b937a38f080da84291400f65ab81ccad4e3ed
Andrii Hlukhov <masterofweb12@gmail.com>
2020-12-07 16:40:21 +0200

    fix build scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+#
+#    Andrii Hlukhov.
+#    masterofweb12@gmail.com
+#    2020
+#
+#    This program is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License
+#    as published by the Free Software Foundation; version 2 of
+#    the License ( https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html ).
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+#    02110-1301  USA
+#
+
 
 MYSQL_SOURCE_VERSION="mysql-8.0.22"
 
