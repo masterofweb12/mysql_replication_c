b790922ad9ff52603ca24c2b14b73b8ad9e5305c
Andrey <masterofweb12@gmail.com>
2022-07-01 20:57:53 +0300

    Update build_test.sh

--- example/C/build_test.sh
+++ example/C/build_test.sh
@@ -38,7 +38,7 @@ fi
 
 gcc -v -g -O0 `mysql_config --cflags` \
  -o ./rpl_test_2 ./rpl_test_2.c \
- -lm -lrt -ldl  -L../../ -lmysql_rpl_listener -L../../openssl/lib -lssl  `mysql_config --libs` -lpthread -ldl -Wl,-rpath,../../
+ -lm -lrt -ldl  -L../../ -lmysql_rpl_listener   `mysql_config --libs` -lpthread -ldl -Wl,-rpath,../../
 
 
 
